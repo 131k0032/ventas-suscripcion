4be3282d1449b87addbe30b53f5b193ee7f260e7
Trainer <mitnick117@gmail.com>
2021-02-06 15:51:07 -0500

    Comit inicial y patron MVC inicial

---
+++ README.md
@@ -0,0 +1,6 @@
+
+## Siguiendo el Proyecto de Ventas por suscripción
+
+El proyecto de ventas por suscripcion es un poryecto de Juan Fernando Urrego de Udemy, se realiza este curso paso a paso hasta su finalización
+
+- [Descarga los archivos fuente en C://xampp/htdocs]
---
+++ index.php
@@ -0,0 +1,8 @@
+<?php 
+
+ require_once "controladores/plantilla.controlador.php";
+
+// Instancia la clase ControladorPlantilla que se encuentra en controladores/plantilla.controlador.php
+ $plantilla = new ControladorPlantilla();
+ // Ejecuta el metodo ctrPlantilla
+ $plantilla->ctrPlantilla();

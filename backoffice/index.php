89faecc009c4097017e56801ba2821590c120c83
Trainer <mitnick117@gmail.com>
2021-02-06 20:41:07 -0500

    18-19. Creando Backoffice y ajustando AdminLTE al MVC

---
+++ backoffice/index.php
@@ -0,0 +1,7 @@
+<?php 
+ require_once "controladores/plantilla.controlador.php";
+
+ // Instancia la clase ControladorPlantilla que se encuentra en controladores/plantilla.controlador.php
+ $plantilla = new ControladorPlantilla();
+ // Ejecuta el metodo ctrPlantilla
+ $plantilla->ctrPlantilla();

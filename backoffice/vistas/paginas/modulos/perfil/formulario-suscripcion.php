909353815deb0b86583ac2d88fa1d53bf4387b99
Trainer <mitnick117@gmail.com>
2021-02-07 20:38:49 -0500

    27. Organizando la pagina de perfil

---
+++ backoffice/vistas/paginas/modulos/perfil/formulario-suscripcion.php
@@ -0,0 +1,101 @@
+<div class="col-12 col-md-8">
+	<div class="card card-primary card-outline">
+		<div class="card-header">
+			<h5 class="m-0 text-uppercase text-secondary">
+				<strong>Suscripción de $10 mensual</strong>
+			</h5>
+		</div>
+
+		<div class="card-body">
+			<h6 class="card-title">Acceso a todo el contenido educativo</h6>
+			<br>
+			<p class="card-text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Obcaecati, voluptas culpa, quia sapiente, eligendi maxime ut dicta similique deserunt excepturi fuga architecto nesciunt, temporibus reiciendis explicabo quo ab? Hic, facere.
+			Modi fugiat, doloremque vero adipisci iusto quo excepturi ad consequuntur, ex minus, molestiae temporibus optio possimus cumque aliquam. Eius, qui aut veritatis itaque hic velit aliquid quis maiores, perspiciatis quo. <a href="plan-compensacion">Plan de compensación</a>
+			</p>
+
+			<div class="form-group">
+				<label for="inputName" class="control-label">Nombre completo</label>
+				<div>
+					<input type="text" class="form-control" id="inputName" value="administrador" readonly>
+				</div>
+			</div>
+
+			<div class="form-group">
+				<label for="inputEmail" class="control-label">Correo electrónico</label>
+				<div>
+					<input type="email" class="form-control" id="inputEmail" value="info@academyoflife.com" readonly>
+				</div>
+			</div>
+
+			<div class="form-group">
+				<label for="inputPatrocinador" class="control-label">Patrocinador</label>
+				<div>
+					<input type="text" class="form-control" id="inputPatrocinador" value="academy-of-life" readonly>
+				</div>
+			</div>
+
+			<div class="form-group">
+				<label for="inputAfiliado" class="control-label">Afiliado</label>
+				<span>(Compartiendo este enlace podrá ganar comisiones, más info en <a href="plan-compensacion">Plan de compensación</a>)</span>
+				<div class="input-group">
+					<div class="input-group-prepend">
+						<span class="p-2 bg-info rounded-left">http://academyoflife.com/</span>
+					</div>
+					<input type="text" class="form-control" id="inputAfiliado" value="academy-of-life" readonly>
+				</div>
+			</div>
+
+			<div class="form-group">
+				<label for="inputPais" class="control-label">Pais</label>
+				<div>
+					<select class="form-control" name="" id="inputPais">
+						<option value="">Seleccionar pais</option>
+					</select>
+				</div>
+			</div>
+
+			<div class="form-group">
+				<label for="inputMovil" class="control-label">Número de telefono</label>
+				<div class="input-group">
+					<div class="input-group-prepend">
+						<span class="p-2 bg-info rounded-left dialCode">+57</span>
+					</div>
+					<input type="text" class="form-control" id="inputMovil">
+				</div>
+			</div>
+
+			<div class="form-group">
+				<label for="tipoRed" class="control-label">Tipo de red</label>
+				<div>
+					<select class="form-control" name="" id="tipoRed">
+						<option value="">Seleccionar tipo de red</option>
+						<option value="uninivel">Red uninivel</option>
+						<option value="binaria">Red binaria</option>
+						<option value="matriz">Red matriz</option>
+					</select>
+				</div>
+			</div>
+
+			<div class="form-group pb-4">
+				<div class="col-sm-offset-2">
+					<div class="checkbox">
+						<input type="checkbox" id="aceptarTerminos">
+						<label for="aceptarTerminos">
+							<span></span>Yo acepto y firmo los <a href="#terminos" data-toggle="collapse">Términos y condiciones</a>
+						</label>
+						<a href="#terminos" data-toggle="collapse" ><span class="float-left float-xl-right text-info"><strong>Ver y firmar términos y condiciones</strong></span></a>
+					</div>	
+				</div>
+			</div>
+			
+
+			<div class="form-group">
+				<div class="col-sm-offset-2">
+					<button type="submit" class="btn btn-dark suscribirse">Suscribirse</button>
+				</div>
+			</div>
+
+		</div>
+
+	</div>
+</div>
---
+++ backoffice/vistas/paginas/modulos/perfil/info-usuario.php
@@ -0,0 +1,25 @@
+<div class="col-12 col-md-4">
+	<div class="card card-info card-outline">
+		<div class="car-body box-profile">
+			<div class="text-center">
+				<img class="profile-user-img img-fluid img-circle" src="vistas/img/usuarios/default/default.png" alt="">
+			</div>
+
+				<h3 class="profile-user-name text-center">
+					Administrador
+				</h3>
+
+				<p class="text-muted text-center">
+					info@academyoflife.com
+				</p>
+				<div class="text-center">
+					<button class="btn btn-primary btn-sm"> Cambiar foto</button>
+					<button class="btn btn-purple btn-sm"> Cambiar contraseña</button>
+				</div>	
+		</div>
+
+			<div class="card-footer">
+				<button class="btn btn-default float-right">Eliminar cuenta</button>
+			</div>
+	</div>
+</div>

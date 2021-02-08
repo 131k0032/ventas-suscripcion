1cbd022aea2ee1162665b6cab568b4844f167e41
Trainer <mitnick117@gmail.com>
2021-02-07 21:07:24 -0500

    28. Maquetando el contrato

--- backoffice/vistas/paginas/modulos/perfil/formulario-suscripcion.php
+++ backoffice/vistas/paginas/modulos/perfil/formulario-suscripcion.php
@@ -87,7 +87,68 @@
 					</div>	
 				</div>
 			</div>
-			
+			<!--==============================
+			=            Contrado            =
+			===============================-->
+			<!-- Para quitar elementos flotantes como float-xl o float-left se usa clearfix -->
+			<div class="clearfix">
+				<div class="collapse pb-4" id="terminos">
+					<div class="card">
+						<div class="card-body">
+							Lorem, ipsum, dolor sit amet consectetur adipisicing elit. Voluptates, sequi, illo! Labore autem, ducimus modi aperiam ratione porro saepe, ipsum in, consectetur, ab qui. Nulla hic non iste unde maiores.
+							Accusantium voluptatum animi alias at tenetur fugit aperiam id quibusdam, adipisci et temporibus assumenda, vel dignissimos, omnis. Fuga et ducimus enim sint, officiis tempore tenetur dicta, delectus quibusdam officia cumque.
+							Mollitia illum reprehenderit, dolor, enim labore ad eveniet, facilis distinctio ab nostrum debitis atque consectetur fuga repudiandae ratione iusto est, reiciendis! Cumque nihil totam, maxime reiciendis dolor! Temporibus, minus, natus.
+							Magnam cum quis numquam, aliquam suscipit impedit eos, repudiandae quam nostrum fugiat voluptatum, accusamus inventore modi vitae magni placeat? Sequi similique tempore porro, assumenda aliquam id reiciendis sunt voluptatum magni.
+						</div>
+
+						<div class="card-header">
+							<a data-toggle="collapse" href="#collapse1" class="card-link">
+								Definiciones del contrato
+							</a>
+						</div>
+
+						<div class="collapse show" id="collapse1" data-parent="#accordion">
+							<div class="card-body">
+								Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis tenetur dicta hic temporibus, repellendus, nihil odio illum culpa distinctio harum exercitationem. Incidunt dolore ad dolor. Fugiat pariatur quae, tenetur blanditiis.
+								Cumque aliquid corporis dolore, exercitationem repellendus hic! Assumenda atque facere velit nemo itaque et enim quis quae rem doloremque rerum eos, similique eius hic quidem explicabo cupiditate inventore neque reprehenderit.
+							</div>
+
+							<!-- Collapse dos -->
+							<div class="card-header">
+								<a class="collapsed card-link" data-toggle="collapse" href="#collapse2">
+									Acuerdos
+								</a>	
+							</div>
+
+							<div class="collapse" id="collapse2" data-parent="#accordion">
+								<div class="card-body">
+									Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facere soluta nihil architecto voluptatem officiis mollitia vitae aut perferendis consectetur. Voluptates sit debitis amet voluptate eius maiores, temporibus odit asperiores facere?
+									Ratione hic illum aut eum ex a. Nisi eos adipisci deleniti quidem amet totam, dolor libero quisquam cupiditate voluptas excepturi recusandae nostrum dicta! Quisquam enim, iste architecto impedit earum doloribus.
+									Enim nulla alias a? Hic aut deserunt iure vero eum fuga qui ducimus, aperiam excepturi ipsam porro. Facilis minus fuga eos hic deleniti quos inventore quam, quae laboriosam sint reprehenderit?
+								</div>
+							</div>
+
+							<!-- Collapse tres -->
+							<div class="card-header">
+								<a class="collapsed card-link" data-toggle="collapse" href="#collapse3">
+									Firma y fecha del contrato
+								</a>	
+							</div>
+
+							<div class="collapse" id="collapse3" data-parent="#accordion">
+								<div class="card-body">
+									Lorem ipsum dolor sit amet consectetur adipisicing elit. firmada el <?php echo date('d/m/Y'); ?>
+									<div class="mb-4" id="signatureparent">
+										<div id="signature"></div>
+									</div>
+									<button type="button" class="repetirFirma btn btn-default btn-sm">Repetir firma</button>
+							</div>
+
+						</div>
+
+					</div>
+				</div>
+			</div>
 
 			<div class="form-group">
 				<div class="col-sm-offset-2">

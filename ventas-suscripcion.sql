ac19c8868bcbbe67a37b0e84f98cb8a12b32593b
Trainer <mitnick117@gmail.com>
2021-02-08 19:08:09 -0500

    34. Inciando creacion de bd

---
+++ ventas-suscripcion.sql
@@ -0,0 +1,84 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 09-02-2021 a las 01:04:37
+-- Versión del servidor: 10.4.17-MariaDB
+-- Versión de PHP: 7.4.13
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Crea Base de datos: `ventas-suscripcion` con cotejamiento utf-8_general_ci
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL,
+  `perfil` text NOT NULL,
+  `nombre` text NOT NULL,
+  `password` text NOT NULL,
+  `email` text NOT NULL,
+  `suscripcion` int(11) NOT NULL,
+  `id_suscripcion` text DEFAULT NULL,
+  `ciclo_pago` int(11) DEFAULT NULL,
+  `vencimiento` date DEFAULT NULL,
+  `verificacion` int(11) NOT NULL,
+  `email_encriptado` text DEFAULT NULL,
+  `foto` text DEFAULT NULL,
+  `enlace_afiliado` text DEFAULT NULL,
+  `patrocinador` text DEFAULT NULL,
+  `paypal` text DEFAULT NULL,
+  `pais` text DEFAULT NULL,
+  `cod_pais` text DEFAULT NULL,
+  `telefono_movil` text DEFAULT NULL,
+  `firma` text DEFAULT NULL,
+  `fecha_contrato` date DEFAULT NULL,
+  `fecha` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `perfil`, `nombre`, `password`, `email`, `suscripcion`, `id_suscripcion`, `ciclo_pago`, `vencimiento`, `verificacion`, `email_encriptado`, `foto`, `enlace_afiliado`, `patrocinador`, `paypal`, `pais`, `cod_pais`, `telefono_movil`, `firma`, `fecha_contrato`, `fecha`) VALUES
+(1, 'admin', 'academy-of-life', 'soloyolose', 'admin@gmail.com', 1, NULL, NULL, NULL, 1, NULL, NULL, 'academy-of-life', NULL, 'mitnick117@hotmail.com', NULL, NULL, NULL, NULL, NULL, '2021-02-09 00:01:56');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
